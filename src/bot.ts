22098d3bdd72ba90b7ef6a7d62678f46e12967eb
Chance Linz <clinz.dev@gmail.com>
2022-01-01 20:35:25 -0800

    basic bot command handling setup

---
+++ src/bot.ts
@@ -0,0 +1,39 @@
+import 'dotenv/config'
+
+import { Client as DiscordClient, Intents, Collection } from 'discord.js'
+
+const commandFiles = ['ping.ts']
+
+const client = new DiscordClient({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] })
+
+const commands = new Collection<string, any>()
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+
+    commands.set(command.data.name, command)
+}
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client?.user?.tag}!`)
+})
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) return
+
+    const command = commands.get(interaction.commandName)
+
+    if (command == null) {
+        return
+    }
+
+    try {
+        await command.execute(interaction)
+    } catch (error) {
+        console.error(error)
+
+        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+    }
+})
+
+client.login(process.env.DISCORD_BOT_TOKEN)
---
+++ src/register-cmds.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config'
+
+import fs from 'fs'
+import { REST } from '@discordjs/rest'
+import { Routes } from 'discord-api-types/v9'
+
+const commands = []
+const commandFiles = ['ping.ts']
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`)
+
+    commands.push(command.data.toJSON())
+}
+
+const token = process.env.DISCORD_BOT_TOKEN as string
+const clientId = process.env.DISCORD_BOT_CLIENT_ID as string
+const guildId = process.env.DISCORD_BOT_TEST_GUILD_ID as string
+
+const rest = new REST({ version: '9' }).setToken(token)
+
+rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+    .then(() => console.log('Successfully registered application commands.'))
+    .catch(console.error)
